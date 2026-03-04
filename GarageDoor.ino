d12e5b5b3f2047d30dd2c38587dac34cf7f5e38e
Frank A. Krueger <fak@praeclarum.org>
2026-03-03 17:49:37 -0800

    Press and release door button

--- GarageDoor.ino
+++ GarageDoor.ino
@@ -8,27 +8,166 @@
 
 // Rename the credentials.sample.h file to credentials.h and 
 // edit it according to your router configuration
-#include "Sectrets.h"
-
-fauxmoESP fauxmo;
+#include "Secrets.h"
 
 // -----------------------------------------------------------------------------
+// Config
+// -----------------------------------------------------------------------------
+
+#define NUM_DOORS           5
 
 #define SERIAL_BAUDRATE     115200
 
-#define LED_YELLOW          4
-#define LED_GREEN           5
-#define LED_BLUE            0
-#define LED_PINK            2
-#define LED_WHITE           15
+#define PIN_RGB_LED         48
 
-#define ID_YELLOW           "yellow lamp"
-#define ID_GREEN            "green lamp"
-#define ID_BLUE             "blue lamp"
-#define ID_PINK             "pink lamp"
-#define ID_WHITE            "white lamp"
+#define PIN_DOOR1           1
+#define PIN_DOOR2           2
+#define PIN_DOOR3           3
+#define PIN_DOOR4           4
+#define PIN_DOOR5           5
+
+#define ID_DOOR1            "Door 1"
+#define ID_DOOR2            "Door 2"
+#define ID_DOOR3            "Door 3"
+#define ID_DOOR4            "Door 4"
+#define ID_DOOR5            "Door 5"
 
 // -----------------------------------------------------------------------------
+// Door Button Emulation
+// -----------------------------------------------------------------------------
+
+enum DoorButtonState {
+    DBS_Released = 0,
+    DBS_Pressing = 1
+};
+
+class DoorButton {
+private:
+    uint8_t pin;
+    DoorButtonState state;
+    bool needsPress;
+    long lastStateTransitionMillis;
+
+public:
+    DoorButton(uint8_t pin)
+        : pin(pin)
+        , state(DBS_Released)
+        , needsPress(false)
+        , lastStateTransitionMillis(millis()) {
+    }
+
+    void setNeedsPress() {
+        needsPress = true;
+    }
+
+    void setup() {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, LOW);
+    }
+
+    void update() {
+        const long now = millis();
+        if ((now - lastStateTransitionMillis) < 250) {
+            return;
+        }
+        switch (state) {
+        case DBS_Pressing:
+            digitalWrite(pin, LOW);
+            state = DBS_Released;
+            lastStateTransitionMillis = now;
+            break;
+        default:
+        case DBS_Released:
+            if (needsPress) {
+                digitalWrite(pin, HIGH);
+                state = DBS_Pressing;
+                needsPress = false;
+                lastStateTransitionMillis = now;
+            }
+            break;
+        }
+    }
+};
+
+DoorButton door1(PIN_DOOR1);
+DoorButton door2(PIN_DOOR2);
+DoorButton door3(PIN_DOOR3);
+DoorButton door4(PIN_DOOR4);
+DoorButton door5(PIN_DOOR5);
+
+DoorButton *doorButtons[5] = {
+  &door1,
+  &door2,
+  &door3,
+  &door4,
+  &door5,
+};
+
+void doorSetup() {
+  for (int i = 0; i < NUM_DOORS; i++) {
+    doorButtons[i]->setup();
+  }
+}
+
+
+// -----------------------------------------------------------------------------
+// Fauxmo
+// -----------------------------------------------------------------------------
+
+fauxmoESP fauxmo;
+
+void fauxmoSetup() {
+    // By default, fauxmoESP creates it's own webserver on the defined port
+    // The TCP port must be 80 for gen3 devices (default is 1901)
+    // This has to be done before the call to enable()
+    fauxmo.createServer(true); // not needed, this is the default value
+    fauxmo.setPort(80); // This is required for gen3 devices
+
+    // You have to call enable(true) once you have a WiFi connection
+    // You can enable or disable the library at any moment
+    // Disabling it will prevent the devices from being discovered and switched
+    fauxmo.enable(true);
+
+    // You can use different ways to invoke alexa to modify the devices state:
+    // "Alexa, turn yellow lamp on"
+    // "Alexa, turn on yellow lamp
+    // "Alexa, set yellow lamp to fifty" (50 means 50% of brightness, note, this example does not use this functionality)
+
+    // Add virtual devices
+    if (NUM_DOORS >= 1) fauxmo.addDevice(ID_DOOR1);
+    if (NUM_DOORS >= 2) fauxmo.addDevice(ID_DOOR2);
+    if (NUM_DOORS >= 3) fauxmo.addDevice(ID_DOOR3);
+    if (NUM_DOORS >= 4) fauxmo.addDevice(ID_DOOR4);
+    if (NUM_DOORS >= 5) fauxmo.addDevice(ID_DOOR5);
+
+    fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
+        
+        // Callback when a command from Alexa is received. 
+        // You can use device_id or device_name to choose the element to perform an action onto (relay, LED,...)
+        // State is a boolean (ON/OFF) and value a number from 0 to 255 (if you say "set kitchen light to 50%" you will receive a 128 here).
+        // Just remember not to delay too much here, this is a callback, exit as soon as possible.
+        // If you have to do something more involved here set a flag and process it in your main loop.
+        
+        Serial.printf("[FAUXMO] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+
+        // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
+        // Otherwise comparing the device_name is safer.
+
+        if (strcmp(device_name, ID_DOOR1)==0) {
+            door1.setNeedsPress();
+        } else if (strcmp(device_name, ID_DOOR2)==0) {
+            door2.setNeedsPress();
+        } else if (strcmp(device_name, ID_DOOR3)==0) {
+            door3.setNeedsPress();
+        } else if (strcmp(device_name, ID_DOOR4)==0) {
+            door4.setNeedsPress();
+        } else if (strcmp(device_name, ID_DOOR5)==0) {
+            door5.setNeedsPress();
+        }
+
+    });
+}
+
 
 // -----------------------------------------------------------------------------
 // Wifi
@@ -52,8 +191,12 @@ void wifiSetup() {
 
     // Connected!
     Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
-
 }
+
+
+// -----------------------------------------------------------------------------
+// Main
+// -----------------------------------------------------------------------------
 
 void setup() {
 
@@ -62,76 +205,19 @@ void setup() {
     Serial.println();
     Serial.println();
 
-    // LEDs
-    pinMode(LED_YELLOW, OUTPUT);
-    pinMode(LED_GREEN, OUTPUT);
-    pinMode(LED_BLUE, OUTPUT);
-    pinMode(LED_PINK, OUTPUT);
-    pinMode(LED_WHITE, OUTPUT);
-    digitalWrite(LED_YELLOW, LOW);
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_BLUE, LOW);
-    digitalWrite(LED_PINK, LOW);
-    digitalWrite(LED_WHITE, LOW);
-
     // Wifi
     wifiSetup();
 
-    // By default, fauxmoESP creates it's own webserver on the defined port
-    // The TCP port must be 80 for gen3 devices (default is 1901)
-    // This has to be done before the call to enable()
-    fauxmo.createServer(true); // not needed, this is the default value
-    fauxmo.setPort(80); // This is required for gen3 devices
+    // Doors
+    doorSetup();
 
-    // You have to call enable(true) once you have a WiFi connection
-    // You can enable or disable the library at any moment
-    // Disabling it will prevent the devices from being discovered and switched
-    fauxmo.enable(true);
-
-    // You can use different ways to invoke alexa to modify the devices state:
-    // "Alexa, turn yellow lamp on"
-    // "Alexa, turn on yellow lamp
-    // "Alexa, set yellow lamp to fifty" (50 means 50% of brightness, note, this example does not use this functionality)
-
-    // Add virtual devices
-    fauxmo.addDevice(ID_YELLOW);
-    fauxmo.addDevice(ID_GREEN);
-    fauxmo.addDevice(ID_BLUE);
-    fauxmo.addDevice(ID_PINK);
-    fauxmo.addDevice(ID_WHITE);
-
-    fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
-        
-        // Callback when a command from Alexa is received. 
-        // You can use device_id or device_name to choose the element to perform an action onto (relay, LED,...)
-        // State is a boolean (ON/OFF) and value a number from 0 to 255 (if you say "set kitchen light to 50%" you will receive a 128 here).
-        // Just remember not to delay too much here, this is a callback, exit as soon as possible.
-        // If you have to do something more involved here set a flag and process it in your main loop.
-        
-        Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-
-        // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
-        // Otherwise comparing the device_name is safer.
-
-        if (strcmp(device_name, ID_YELLOW)==0) {
-            digitalWrite(LED_YELLOW, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_GREEN)==0) {
-            digitalWrite(LED_GREEN, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_BLUE)==0) {
-            digitalWrite(LED_BLUE, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_PINK)==0) {
-            digitalWrite(LED_PINK, state ? HIGH : LOW);
-        } else if (strcmp(device_name, ID_WHITE)==0) {
-            digitalWrite(LED_WHITE, state ? HIGH : LOW);
-        }
-
-    });
-
+    // Fauxmo
+    fauxmoSetup();
 }
 
 void loop() {
 
     fauxmo.handle();
-    // fauxmo.setState(ID_YELLOW, true, 255);
+    // fauxmo.setState(ID_DOOR1, true, 255);
 
 }
