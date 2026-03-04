f448d85ed9ee4da230e02ce100883ae0cd9b1e4c
Frank A. Krueger <fak@praeclarum.org>
2026-03-04 11:23:31 -0800

    Switch to matter

--- GarageDoor.ino
+++ GarageDoor.ino
@@ -1,10 +1,11 @@
 #include <Arduino.h>
+#include <Matter.h>
+#include <Preferences.h>
 #ifdef ESP32
-    #include <WiFi.h>
+  #include <WiFi.h>
 #else
-    #include <ESP8266WiFi.h>
+  #include <ESP8266WiFi.h>
 #endif
-#include "fauxmoESP.h"
 
 // Rename the credentials.sample.h file to credentials.h and 
 // edit it according to your router configuration
@@ -14,11 +15,16 @@
 // Config
 // -----------------------------------------------------------------------------
 
-#define NUM_DOORS           5
+#define NUM_DOORS           1
 
 #define SERIAL_BAUDRATE     115200
 
-#define PIN_RGB_LED         48
+#ifdef RGB_BUILTIN
+const uint8_t ledPin = RGB_BUILTIN;
+#else
+const uint8_t ledPin = 2;  // Set your pin here if your board has not defined RGB_BUILTIN
+#warning "Do not forget to set the RGB LED pin"
+#endif
 
 #define PIN_DOOR1           1
 #define PIN_DOOR2           2
@@ -33,8 +39,20 @@
 #define ID_DOOR5            "Door 5"
 
 // -----------------------------------------------------------------------------
-// Door Button Emulation
+// Door
 // -----------------------------------------------------------------------------
+
+const uint8_t LIFT_SECONDS = 12;
+
+// Window covering limits
+// Lift limits in centimeters (physical position)
+const uint16_t MAX_LIFT = 200;  // Maximum lift position (fully open)
+const uint16_t MIN_LIFT = 0;    // Minimum lift position (fully closed)
+
+// Current window covering state
+// These will be initialized in setup() based on installed limits and saved percentages
+uint16_t currentLift = 0;  // Lift position in cm
+uint8_t currentLiftPercent = 100;
 
 enum DoorButtonState {
     DBS_Released = 0,
@@ -63,6 +81,7 @@ public:
     void setup() {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, LOW);
+        Serial.printf("[DOOR] Button GPIO #%d\n", pin);
     }
 
     void update() {
@@ -109,63 +128,158 @@ void doorSetup() {
   }
 }
 
+void visualizeWindowBlinds(uint8_t liftPercent) {
+#ifdef RGB_BUILTIN
+  // Use RGB LED to visualize lift position (brightness)
+  float brightness = (float)liftPercent / 100.0f;  // 0.0 to 1.0
+  uint8_t red = (uint8_t)(255 * brightness);
+  uint8_t blue = red;
+  uint8_t green = red;
+  rgbLedWrite(ledPin, red, green, blue);
+#else
+  // For non-RGB boards, just use brightness
+  uint8_t brightnessValue = map(liftPercent, 0, 100, 0, 255);
+  analogWrite(ledPin, brightnessValue);
+#endif
+}
+
 
 // -----------------------------------------------------------------------------
-// Fauxmo
+// Matter
 // -----------------------------------------------------------------------------
 
-fauxmoESP fauxmo;
+Preferences matterPref;
+const char *liftPercentPrefKey = "LiftPercent";
+// List of Matter Endpoints for this Node
+// Window Covering Endpoint
+MatterWindowCovering WindowBlinds;
 
-void fauxmoSetup() {
-    // By default, fauxmoESP creates it's own webserver on the defined port
-    // The TCP port must be 80 for gen3 devices (default is 1901)
-    // This has to be done before the call to enable()
-    fauxmo.createServer(true); // not needed, this is the default value
-    fauxmo.setPort(80); // This is required for gen3 devices
+bool goToLiftPercentage(uint8_t liftPercent) {
+  // update Lift operational state
+  if (liftPercent > currentLiftPercent) {
+    // Set operational status to OPEN
+    WindowBlinds.setOperationalState(MatterWindowCovering::LIFT, MatterWindowCovering::MOVING_UP_OR_OPEN);
+  }
+  if (liftPercent < currentLiftPercent) {
+    // Set operational status to CLOSE
+    WindowBlinds.setOperationalState(MatterWindowCovering::LIFT, MatterWindowCovering::MOVING_DOWN_OR_CLOSE);
+  }
 
-    // You have to call enable(true) once you have a WiFi connection
-    // You can enable or disable the library at any moment
-    // Disabling it will prevent the devices from being discovered and switched
-    fauxmo.enable(true);
+  // This is where you would trigger your motor to go towards liftPercent
+  // For simulation, we update instantly
+  // Calculate absolute position based on installed limits
+  uint16_t openLimit = WindowBlinds.getInstalledOpenLimitLift();
+  uint16_t closedLimit = WindowBlinds.getInstalledClosedLimitLift();
 
-    // You can use different ways to invoke alexa to modify the devices state:
-    // "Alexa, turn yellow lamp on"
-    // "Alexa, turn on yellow lamp
-    // "Alexa, set yellow lamp to fifty" (50 means 50% of brightness, note, this example does not use this functionality)
+  // Linear interpolation: 0% = openLimit, 100% = closedLimit
+  if (openLimit < closedLimit) {
+    currentLift = openLimit + ((closedLimit - openLimit) * liftPercent) / 100;
+  } else {
+    currentLift = openLimit - ((openLimit - closedLimit) * liftPercent) / 100;
+  }
+  currentLiftPercent = liftPercent;
+  Serial.printf("Moving lift to %d%% (position: %d cm)\r\n", currentLiftPercent, currentLift);
 
-    // Add virtual devices
-    if (NUM_DOORS >= 1) fauxmo.addDevice(ID_DOOR1);
-    if (NUM_DOORS >= 2) fauxmo.addDevice(ID_DOOR2);
-    if (NUM_DOORS >= 3) fauxmo.addDevice(ID_DOOR3);
-    if (NUM_DOORS >= 4) fauxmo.addDevice(ID_DOOR4);
-    if (NUM_DOORS >= 5) fauxmo.addDevice(ID_DOOR5);
+  // Update CurrentPosition to reflect actual position (setLiftPercentage now only updates CurrentPosition)
+  WindowBlinds.setLiftPercentage(currentLiftPercent);
 
-    fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
-        
-        // Callback when a command from Alexa is received. 
-        // You can use device_id or device_name to choose the element to perform an action onto (relay, LED,...)
-        // State is a boolean (ON/OFF) and value a number from 0 to 255 (if you say "set kitchen light to 50%" you will receive a 128 here).
-        // Just remember not to delay too much here, this is a callback, exit as soon as possible.
-        // If you have to do something more involved here set a flag and process it in your main loop.
-        
-        Serial.printf("[FAUXMO] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+  // Set operational status to STALL when movement is complete
+  WindowBlinds.setOperationalState(MatterWindowCovering::LIFT, MatterWindowCovering::STALL);
 
-        // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
-        // Otherwise comparing the device_name is safer.
+  // Store state
+  matterPref.putUChar(liftPercentPrefKey, currentLiftPercent);
 
-        if (strcmp(device_name, ID_DOOR1)==0) {
-            door1.setNeedsPress();
-        } else if (strcmp(device_name, ID_DOOR2)==0) {
-            door2.setNeedsPress();
-        } else if (strcmp(device_name, ID_DOOR3)==0) {
-            door3.setNeedsPress();
-        } else if (strcmp(device_name, ID_DOOR4)==0) {
-            door4.setNeedsPress();
-        } else if (strcmp(device_name, ID_DOOR5)==0) {
-            door5.setNeedsPress();
-        }
+  return true;
+}
 
-    });
+bool stopMotor() {
+  // Motor can be stopped while moving cover toward current target
+  Serial.println("Stopping window covering motor");
+
+  // Update CurrentPosition to reflect actual position when stopped
+  WindowBlinds.setLiftPercentage(currentLiftPercent);
+
+  // Set operational status to STALL for both lift
+  WindowBlinds.setOperationalState(MatterWindowCovering::LIFT, MatterWindowCovering::STALL);
+
+  return true;
+}
+
+void matterSetup() {
+  // Initialize Matter EndPoint
+  matterPref.begin("MatterPrefs", false);
+  // default lift percentage is 0% (fully closed) if not stored before
+  uint8_t lastLiftPercent = matterPref.getUChar(liftPercentPrefKey, 0);
+
+  WindowBlinds.begin(lastLiftPercent, 0, MatterWindowCovering::ROLLERSHADE_EXTERIOR);
+
+  // Configure installed limits for lift
+  WindowBlinds.setInstalledOpenLimitLift(MIN_LIFT);
+  WindowBlinds.setInstalledClosedLimitLift(MAX_LIFT);
+
+  // Initialize current positions based on percentages and installed limits
+  uint16_t openLimitLift = WindowBlinds.getInstalledOpenLimitLift();
+  uint16_t closedLimitLift = WindowBlinds.getInstalledClosedLimitLift();
+  currentLiftPercent = lastLiftPercent;
+  if (openLimitLift < closedLimitLift) {
+    currentLift = openLimitLift + ((closedLimitLift - openLimitLift) * lastLiftPercent) / 100;
+  } else {
+    currentLift = openLimitLift - ((openLimitLift - closedLimitLift) * lastLiftPercent) / 100;
+  }
+
+  Serial.printf(
+    "Window Covering limits configured: Lift [%d-%d cm]\r\n",
+    WindowBlinds.getInstalledOpenLimitLift(),
+    WindowBlinds.getInstalledClosedLimitLift());
+  Serial.printf("Initial positions: Lift=%d cm (%d%%)\r\n", currentLift, currentLiftPercent);
+
+  // Set callback functions
+  // WindowBlinds.onOpen(fullOpen);
+  // WindowBlinds.onClose(fullClose);
+  WindowBlinds.onGoToLiftPercentage(goToLiftPercentage);
+  WindowBlinds.onStop(stopMotor);
+
+  // Generic callback for Lift or Tilt change
+  WindowBlinds.onChange([](uint8_t liftPercent, uint8_t tiltPercent) {
+    Serial.printf("Window Covering changed: Lift=%d%%, Tilt=%d%%\r\n", liftPercent, tiltPercent);
+    visualizeWindowBlinds(liftPercent);
+    return true;
+  });
+
+  // Matter beginning - Last step, after all EndPoints are initialized
+  Matter.begin();
+  // This may be a restart of a already commissioned Matter accessory
+  if (Matter.isDeviceCommissioned()) {
+    Serial.println("Matter Node is commissioned and connected to the network. Ready for use.");
+    Serial.printf("Initial state: Lift=%d%%\r\n", WindowBlinds.getLiftPercentage());
+    // Update visualization based on initial state
+    visualizeWindowBlinds(WindowBlinds.getLiftPercentage());
+  }
+}
+
+void matterUpdate() {
+  // Check Matter Window Covering Commissioning state, which may change during execution of loop()
+  if (!Matter.isDeviceCommissioned()) {
+    Serial.println("");
+    Serial.println("Matter Node is not commissioned yet.");
+    Serial.println("Initiate the device discovery in your Matter environment.");
+    Serial.println("Commission it to your Matter hub with the manual pairing code or QR code");
+    Serial.printf("Manual pairing code: %s\r\n", Matter.getManualPairingCode().c_str());
+    const auto qrCodeUrl = Matter.getOnboardingQRCodeUrl();
+    Serial.printf("QR code URL: %s\r\n", qrCodeUrl.c_str());
+    // waits for Matter Window Covering Commissioning.
+    uint32_t timeCount = 0;
+    while (!Matter.isDeviceCommissioned()) {
+      delay(100);
+      if ((timeCount++ % 50) == 0) {  // 50*100ms = 5 sec
+        Serial.println("Matter Node not commissioned yet. Waiting for commissioning.");
+      }
+    }
+    Serial.printf("Initial state: Lift=%d%%\r\n", WindowBlinds.getLiftPercentage());
+    // Update visualization based on initial state
+    visualizeWindowBlinds(WindowBlinds.getLiftPercentage());
+    Serial.println("Matter Node is commissioned and connected to the network. Ready for use.");
+  }
 }
 
 
@@ -211,13 +325,12 @@ void setup() {
     // Doors
     doorSetup();
 
-    // Fauxmo
-    fauxmoSetup();
+    // Matter
+    matterSetup();
 }
 
 void loop() {
 
-    fauxmo.handle();
-    // fauxmo.setState(ID_DOOR1, true, 255);
+    matterUpdate();
 
 }
